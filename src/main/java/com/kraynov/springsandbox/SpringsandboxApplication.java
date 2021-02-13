2a5f8826483d60ea9eaf496c986e65816913cf98
KraynovPS <Kraynov.P.S@sberbank.ru>
2021-02-13 21:55:01 +0400

    Initial commit

---
+++ src/main/java/com/kraynov/springsandbox/SpringsandboxApplication.java
@@ -0,0 +1,13 @@
+package com.kraynov.springsandbox;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringsandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringsandboxApplication.class, args);
+    }
+
+}
